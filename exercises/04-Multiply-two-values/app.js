85e071741672db7cf8d19440e789616aea9f34b5
davila29v <davila29v@hotmail.com>
2021-03-22 22:38:52 +0000

    avances

--- exercises/04-Multiply-two-values/app.js
+++ exercises/04-Multiply-two-values/app.js
@@ -1,1 +1,7 @@
-// Your code below:
+
+
+var variablesAreCool = (num1, num2) => {
+  return (num1 * num2);
+};
+
+console.log (variablesAreCool(2345, 7323));
--- exercises/11-Create-a-new-function/app.js
+++ exercises/11-Create-a-new-function/app.js
@@ -1,3 +1,18 @@
 /* This exercise is almost identical to 15-Random numbers - consider merging them here and using another concept for 15? */
 
+// solucion del demo:
+// function generateRandom(){
+//     return Math.floor(Math.random() * 10);
+// }
+// console.log(generateRandom())
+
+
+
 // Declare your function here
+function getRandomInt()
+{
+var randomNumber = Math.floor((Math.random() * 9));
+return randomNumber	
+}
+
+console.log(getRandomInt());

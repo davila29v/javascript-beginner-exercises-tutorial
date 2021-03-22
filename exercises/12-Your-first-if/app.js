4dadb9e0a0aeb5184c4a6e6b9951c372f9ec1a49
davila29v <davila29v@hotmail.com>
2021-03-22 23:43:56 +0000

    avance

--- exercises/12-Your-first-if/app.js
+++ exercises/12-Your-first-if/app.js
@@ -1,3 +1,26 @@
 var total = prompt('How many km are left to go?');
 
 // Your code below:
+function testNum () {
+  let result;
+  if (a > 0) {
+    result = "We still have a bit of driving left to go";
+  } else if {
+    result = "We'll be there in 5 minutes";
+  }
+
+  return result;
+}
+
+console.log(testNum(-5));
+// expected output: "NOT positive"
+
+
+
+if (x > 50) {
+  /* do something */
+} else if (x > 5) {
+  /* do something */
+} else {
+  /* do something */
+}

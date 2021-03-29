4960604b953a8e81df3e8113f26b1c5e8d8a662c
davila29v <davila29v@hotmail.com>
2021-03-29 19:32:50 +0000

    completo

--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -1,5 +1,14 @@
-function milkSong() {
-    let string = "";
+//function milkSong() {
+
+    for (let i = 99; i > 0; i--){
+        if (i > 1) {
+        console.log (`${i} bottles of milk on the wall, ${i} bottles of milk. Take one down and pass it around, ${i-1} bottles of milk on the wall.`);
+    }
+    else {
+        console.log (`${i} bottle of milk on the wall, ${i} bottle of milk. Take one down and pass it around, no more bottles of milk on the wall. No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, ${i+ 98} bottles of milk on the wall.`);
+    }
+    }
+ /*   let string = "";
     for (let i = 0; i < 8; i++) {
         if (i == 1) { string = string + "99 bottles of milk on the wall, 99 bottles of milk."; }
         if (i == 2) { string = string + "Take one down and pass it around, 98 bottles of milk on the wall."; }
@@ -8,13 +17,13 @@ function milkSong() {
        if (i == 6) { string = string + "1 bottle of milk on the wall, 1 bottle of milk.";}
        if (i == 7) { string = string + "Take one down and pass it around, no more bottles of milk on the wall.";}
        if (i == 8) { string = string + "No more bottles of milk on the wall, no more bottles of milk.";}
-        if (i == 9) { string = string + "Go to the store and buy some more, 99 bottles of milk on the wall.";}
+    if (i == 9) { string = string + "Go to the store and buy some more, 99 bottles of milk on the wall.";}
        
     }
     return string;
 };
-
+*/
 
 //Your code above ^^^
 
-console.log(milkSong());
+//console.log(milkSong());
